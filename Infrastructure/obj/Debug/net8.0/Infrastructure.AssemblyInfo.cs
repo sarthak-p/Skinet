0d6b823a377ba08e8b6920002ee731ecfcad3052
Sarthak Patipati <pusarthak@gmail.com>
2024-08-29 08:28:41 -0500

    Created the Product Entity

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712ea8471e9516f1adfeef465251eb614de71c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5fb8c318b918772584a2ec06477b6a0e2d2d100")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

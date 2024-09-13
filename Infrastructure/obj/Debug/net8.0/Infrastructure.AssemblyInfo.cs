5ccd7115b0f4cfd2f2b1012a5ffd14e9a11ea2ed
Sarthak Patipati <pusarthak@gmail.com>
2024-09-13 08:04:34 -0500

    Filtered the products by brand and type

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3208b1a68aa9bace4e2ba9961141ab6321bbcd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bc2db9a8f3ac0e5ec4306b92270dd554f4d737")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

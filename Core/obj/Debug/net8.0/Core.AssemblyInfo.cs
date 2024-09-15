5b11b7cad63907f32fe5de13adef5896c69f6b4a
Sarthak Patipati <pusarthak@gmail.com>
2024-09-15 10:06:14 -0500

    Added sorting by price

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bc2db9a8f3ac0e5ec4306b92270dd554f4d737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccd7115b0f4cfd2f2b1012a5ffd14e9a11ea2ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

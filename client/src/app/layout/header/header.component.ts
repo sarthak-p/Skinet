77893394d56f4684ac558a24914c3a972520ad23
Sarthak Patipati <pusarthak@gmail.com>
2024-11-02 13:22:01 -0500

    Added a Header component

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })

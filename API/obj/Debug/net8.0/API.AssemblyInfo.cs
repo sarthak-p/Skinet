79e7a9a7ac2cecebedfb3c3e5c6c40dcb816f8e4
Sarthak Patipati <pusarthak@gmail.com>
2024-11-25 14:49:01 -0600

    Fixed CERT and styling issues

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ec02fa193bdeb68dd92f91caeebb937b884586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa3be8ae2343a980cfa0881a6519ba4c4529441")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

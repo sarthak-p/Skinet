5608bc7310e4c485cadcb9e20f5749205c0d108b
Sarthak Patipati <pusarthak@gmail.com>
2024-12-07 12:25:33 -0600

    Added the filtering functionality using Material Dialog 2

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -3,13 +3,19 @@ import { Product } from '../../shared/models/products';
 import { ShopService } from '../../core/services/shop.service';
 import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
+import { MatDialog } from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
   imports: [
     MatCard,
-    ProductItemComponent
+    MatButton,
+    ProductItemComponent,
+    MatIcon
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
@@ -17,7 +23,11 @@ import { ProductItemComponent } from "./product-item/product-item.component";
 export class ShopComponent implements OnInit {
 
   private shopService = inject(ShopService);
+  private dialogService = inject(MatDialog);
   products: Product[] = [];
+  selectedBrands: string[] = [];
+  selectedTypes: string[] = [];
+
 
   ngOnInit(): void {
     this.initializeShop(); 
@@ -29,6 +39,26 @@ export class ShopComponent implements OnInit {
     this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error),
+    })
+  }
+
+  openFilterDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '500px',
+      data: {
+        selectedBrands: this.selectedBrands,
+        selectedTypes: this.selectedTypes
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => ({
+      next: result => {
+        if (result) {
+          console.log(result); 
+          this.selectedBrands = result.selectedBrands;
+          this.selectedTypes = result.selectedTypes;
+          // apply filters
+        }
+      }
     })
   }
 }

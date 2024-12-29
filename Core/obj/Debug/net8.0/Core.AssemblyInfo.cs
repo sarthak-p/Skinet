4f15f7975253dd4929d843274f5e9977441d73b0
Sarthak Patipati <pusarthak@gmail.com>
2024-12-29 17:28:58 -0600

    Created a NotFound and Server Error component

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2050a18b61aadda823057f65a64006e161ab76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a32219927da67bf2a4cd4003e918e9b6ca63f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

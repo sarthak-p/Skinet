b683330b3589da6529df5a72401598a38e48a5ac
Sarthak Patipati <pusarthak@gmail.com>
2025-01-01 12:48:23 -0600

    Created an HTTP Interceptor for handling API errors

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a32219927da67bf2a4cd4003e918e9b6ca63f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f15f7975253dd4929d843274f5e9977441d73b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,13 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(), 
-    provideHttpClient(withFetch())
+    provideHttpClient(withInterceptors([errorInterceptor]))
   ]
 };

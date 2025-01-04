0078ef04f2b7ca304ce452e509c3b440d0b92b6b
Sarthak Patipati <pusarthak@gmail.com>
2025-01-03 18:57:36 -0600

    Handled validation errors from the API

--- client/src/app/features/test-error/test-error.component.ts
+++ client/src/app/features/test-error/test-error.component.ts
@@ -13,9 +13,10 @@ import { MatButton } from '@angular/material/button';
 })
 export class TestErrorComponent {
   baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
+  private http = inject(HttpClient); 
+  validationErrors?: string[];
 
-  get404Error() {
+  get404Error() { 
     this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
       next: response => console.log(response),
       error: error => console.log(error)
@@ -46,7 +47,7 @@ export class TestErrorComponent {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => this.validationErrors = error
     })
   }
 }

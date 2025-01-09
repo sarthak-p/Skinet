3cdce5eac7ce4dcd95aed525579b9c9cb4ad9353
Sarthak Patipati <pusarthak@gmail.com>
2025-01-08 18:57:34 -0600

    Added an HTTP Interceptor for loading

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,12 +4,14 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(), 
-    provideHttpClient(withInterceptors([errorInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))
+]
 };
+ 

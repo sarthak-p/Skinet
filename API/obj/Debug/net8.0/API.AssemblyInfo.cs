da73d35c08ceaf4118b47b207b8b3742af683c73
Sarthak Patipati <pusarthak@gmail.com>
2025-02-18 12:43:38 -0600

    Created the login form

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45ab8be2b1d06f0de5f6dab9a2c57d849c5fd3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6275a7fdc95fa8f5705e2e1f1887654155ce1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

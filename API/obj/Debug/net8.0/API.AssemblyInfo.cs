3320a1231e51f6cbe1177739c980a1d60a3fc24c
Sarthak Patipati <pusarthak@gmail.com>
2025-02-27 13:00:16 -0600

    Created an auth guard

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e8f9dce41e38d8d142eb95894cf907264b43e12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c3f2dbf574b9098612a1cf3694963582b14384")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

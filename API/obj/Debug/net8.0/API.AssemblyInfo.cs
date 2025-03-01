4c380c740f5ef6df565561d4ee4c9eac3168d8fd
Sarthak Patipati <pusarthak@gmail.com>
2025-03-01 09:44:40 -0600

    Added empty cart guard

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c3f2dbf574b9098612a1cf3694963582b14384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3320a1231e51f6cbe1177739c980a1d60a3fc24c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
